2c2ea6aceaeae1c352a063f33cfa73a290da67a9
Vikramcool <95308488+Vikramcool@users.noreply.github.com>
2022-02-07 14:06:06 +0530

    Add files via upload

---
+++ num0to10.java
@@ -0,0 +1,26 @@
+
+     public class num0to10
+  
+   {
+
+     public static void main(String   args[])
+    
+    {
+ 
+       int i=0;
+ 
+       while(i<=10)
+ 
+       {
+    
+        System.out.println(i);
+      
+        i++;
+   
+    
+       }
+
+     }
+
+
+   }
